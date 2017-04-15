2b33722cca08a00800b555b754d6b171946c7e6d
Theo Li <bbtfrr@gmail.com>
2017-04-15 23:42:40 +0800

    add get / set item operators

--- lib/pybind/wrapper/attr_accessor.rb
+++ lib/pybind/wrapper/attr_accessor.rb
@@ -11,13 +11,39 @@ module PyBind
 
     def set_attr(name, value)
       value = TypeCast.from_ruby(value)
-      res = LibPython.PyObject_SetAttrString(__pyref__, name.to_s, value)
-      raise PyError.fetch if res == -1
+      raise PyError.fetch if LibPython.PyObject_SetAttrString(__pyref__, name.to_s, value) == -1
       self
     end
 
     def has_attr?(name)
       LibPython.PyObject_HasAttrString(__pyref__, name.to_s) == 1
     end
+
+    def [](*indices)
+      if indices.length == 1
+        indices = indices[0]
+      else
+        indices = PyCall.tuple(*indices)
+      end
+      pykey = TypeCast.from_ruby(indices)
+      value = LibPython.PyObject_GetItem(__pyref__, pykey)
+      raise PyError.fetch if value.null?
+      value.to_ruby
+    end
+
+    def []=(*indices_and_value)
+      value = indices_and_value.pop
+      indices = indices_and_value
+      if indices.length == 1
+        indices = indices[0]
+      else
+        indices = PyCall.tuple(*indices)
+      end
+      pykey = TypeCast.from_ruby(indices)
+      value = TypeCast.from_ruby(value)
+      raise PyError.fetch if LibPython.PyObject_SetItem(__pyref__, pykey, value) == -1
+      self
+    end
+
   end
 end
